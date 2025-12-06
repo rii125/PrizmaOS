b56485955a5158296b669498af3d700aea6204ba
rii_125 <rii125.idd@gmail.com>
2025-12-06 21:57:38 +0900

    Java25の文法に対応＆アノテーションの追加

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,11 +6,16 @@ import window.panel.Import;
 import javax.swing.*;
 
 public class Main extends JFrame {
-    public static void main(String[] args) throws InterruptedException {
+    @version("0.0.0")
+    static void main() throws InterruptedException {
         Import imp = new Import("Now importing...");
         imp.popUp("Now importing...", 300, 200);
         Thread.sleep(3000);
         imp.close();
         Window window = new Window("PrizmaOS", 1400, 900);
+    }
+
+    public @interface version {
+        String value();
     }
 }
