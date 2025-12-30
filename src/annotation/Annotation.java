08b88152526a7b663f506441017c5f9c4b5f0431
rii_125 <rii125.idd@gmail.com>
2025-12-30 14:16:06 +0900

    アノテーション専用javaファイルを作成

---
+++ src/annotation/Annotation.java
@@ -0,0 +1,7 @@
+package annotation;
+
+public class Annotation {
+    public @interface version {
+        String value();
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,13 +5,11 @@ import static java.lang.System.out;
 
 import javax.swing.*;
 
+import static annotation.Annotation.*;
+
 public class Main extends JFrame {
     @version("0.0.0")
-    static void main() {
+     static void main() {
         Window window = new Window("PrizmaOS", 1400, 900);
-    }
-
-    private @interface version {
-        String value();
     }
 }
